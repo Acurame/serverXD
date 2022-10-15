783644e12b5652e0750d2fad864cb951b60c1bae
Acurame <acuramee@gmail.com>
2022-10-15 13:14:04 -0600

    sin confirmacion

--- servidor_apache.sh
+++ servidor_apache.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo apt update
-sudo apt install apache2
-sudo apt install mysql-server
-systemctl reload apache2
+sudo apt install apache2 -y
+sudo apt install mysql-server -y
+systemctl reload apache2 -y
 sudo apt install software-properties-common apt-transport-https -y
 sudo add-apt-repository ppa:ondrej/php -y
-sudo apt install php8.1 libapache2-mod-php8.1
-sudo apt install php-mysql php-xml php-gd php-mbstring php-mysql php-bcmath curl php-cli php-mbstring git unzip
-sudo apt install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath
+sudo apt install php8.1 libapache2-mod-php8.1 -y
+sudo apt install php-mysql php-xml php-gd php-mbstring php-mysql php-bcmath curl php-cli php-mbstring git unzip -y
+sudo apt install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath -y
 sudo apt update
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
