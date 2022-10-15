5449f83086853b31f6c3bb5893fd35a961711299
Acurame <acuramee@gmail.com>
2022-10-15 12:45:09 -0600

    simplificando sript agrando node actualizado

--- servidor_apache.sh
+++ servidor_apache.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-sudo su
 sudo apt update
-sudo apt install apache2 --no
-systemctl reload apache2
+sudo apt install apache2
 sudo apt install mysql-server
-sudo apt install php
-sudo apt install libapache2-mod-php
-sudo apt install php-mysql
-sudo apt-get update
-sudo apt install php-xml
-sudo apt install php-gd
-sudo apt install php-mbstring
-sudo apt install php-mysql
-sudo apt install php-bcmath
+systemctl reload apache2
+sudo apt install software-properties-common apt-transport-https -y
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt install php8.1 libapache2-mod-php8.1
+sudo apt install php-mysql php-xml php-gd php-mbstring php-mysql php-bcmath curl php-cli php-mbstring git unzip
+sudo apt install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath
 sudo apt update
-sudo apt install curl php-cli php-mbstring git unzip
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-sudo apt-get install phpmyadmin
-sudo echo -e "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
-sudo apt install nodejs npm
+#sudo apt-get install phpmyadmin
+#sudo echo -e "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+sudo ufw allow https
+sudo ufw allow http
+sudo ufw allow 'Apache'
+sudo ufw enable
+sudo ufw status
