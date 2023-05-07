728c044dd6077c99fede5e937bfb8bb8fd846f7c
Acurame <acuramee@gmail.com>
2023-05-06 21:24:23 -0600

    conbinando lineas que conpartia paquetes

--- servidor_apache.sh
+++ servidor_apache.sh
@@ -7,8 +7,7 @@ systemctl reload apache2 -y
 sudo apt install software-properties-common apt-transport-https -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt install php8.1 libapache2-mod-php8.1 -y
-sudo apt install php-mysql php-xml php-gd php-mbstring php-mysql php-bcmath curl php-cli php-mbstring git unzip -y
-sudo apt install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath -y
+sudo apt install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath curl git unzip -y
 sudo apt update
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
