4a42c89715421902fb7c092d9d5858281dd79d4c
Acurame <acuramee@gmail.com>
2023-05-16 19:23:57 -0600

    cambios en npm para la version 20

--- servidor_apache.sh
+++ servidor_apache.sh
@@ -6,17 +6,18 @@ sudo a2enmod rewrite
 systemctl reload apache2 -y
 sudo apt install software-properties-common apt-transport-https -y
 sudo add-apt-repository ppa:ondrej/php -y
-sudo apt install php8.1 libapache2-mod-php8.1 -y
+sudo apt install php libapache2-mod-php -y
 sudo apt install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath curl git unzip -y
 sudo apt update
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 #sudo apt-get install phpmyadmin
 #sudo echo -e "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
-sudo ufw allow https
-sudo ufw allow http
-sudo ufw allow 'Apache'
-sudo ufw enable
-sudo ufw status
+#activar si vamos a usar un servidor real 
+#sudo ufw allow https
+#sudo ufw allow http
+#sudo ufw allow 'Apache'
+#sudo ufw enable
+#sudo ufw status
