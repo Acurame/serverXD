a0eb5e4e6185be82f03c206c50fd653afa23b211
Acurame <acuramee@gmail.com>
2023-11-24 19:25:37 -0600

    una y que se me fue

--- servidor_apache.sh
+++ servidor_apache.sh
@@ -19,7 +19,7 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=21
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install nodejs -y
 #activar si vamos a usar un servidor real 
 #sudo ufw allow https
