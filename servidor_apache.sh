ccb43d099a813c3960299546008ac862bb98ac00
Acurame <acuramee@gmail.com>
2023-11-24 18:49:01 -0600

    actualizacion de node

--- servidor_apache.sh
+++ servidor_apache.sh
@@ -13,9 +13,13 @@ curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 #sudo apt-get install phpmyadmin
 #sudo echo -e "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
-sudo apt-get install -y ca-certificates curl gnupg -y
-sudo mkdir -p /etc/apt/keyrings -y
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=21
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
 sudo apt-get install nodejs -y
 #activar si vamos a usar un servidor real 
 #sudo ufw allow https
